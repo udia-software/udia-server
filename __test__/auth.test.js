babae1c576dda416aa366db4fbd3887ec26f4f3e
Alexander Wong <admin@alexander-wong.com>
2017-11-16 22:17:03 -0700

    fuckit, process.exit()

--- __test__/auth.test.js
+++ __test__/auth.test.js
@@ -2,18 +2,23 @@ const Auth = require("../src/modules/Auth");
 const UserManager = require("../src/modules/UserManager");
 const testHelper = require("./testhelper");
 
-beforeEach(async() => {
-  return await testHelper.initializeTestState();
+beforeEach(async done => {
+  await testHelper.initializeTestState();
+  await done();
 });
 
+// fuck it
+afterAll(() => setTimeout(() => process.exit(), 1000));
+
 describe("Auth Module", () => {
-  it("should hash a raw password", async () => {
+  it("should hash a raw password", async done => {
     const rawPassword = "Secret123";
     const hashPassword = await Auth.hashPassword(rawPassword);
     expect(rawPassword).not.toBe(hashPassword);
+    await done();
   });
 
-  it("should authenticate a valid user", async () => {
+  it("should authenticate a valid user", async done => {
     const name = "Test User";
     const rawPassword = "Secret123";
     const email = "test@test.com";
@@ -37,6 +42,9 @@ describe("Auth Module", () => {
       { headers: { authorization: token } },
       userManager
     );
-    expect(await userManager.getUserById(newUserData.id)).toEqual(loggedUserData);
+    expect(await userManager.getUserById(newUserData.id)).toEqual(
+      loggedUserData
+    );
+    await done();
   });
 });
