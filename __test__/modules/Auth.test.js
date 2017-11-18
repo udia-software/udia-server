9922318739c59ae9140735795aa85af349b31887
Alexander Wong <admin@alexander-wong.com>
2017-11-18 00:26:55 -0700

    removed test console logging

--- __test__/modules/Auth.test.js
+++ __test__/modules/Auth.test.js
@@ -3,29 +3,24 @@ const UserManager = require("../../src/modules/UserManager");
 const testHelper = require("../testhelper");
 
 beforeEach(async done => {
-  console.log('init test state');
   await testHelper.initializeTestState();
   done();
 });
 
 afterAll(async done => {
-  console.log('close all test states')
   await testHelper.tearDownTestState(true);
   done();
 });
 
 describe("Auth Module", () => {
   it("should hash a raw password", async done => {
-    console.log('pw start');
     const rawPassword = "Secret123";
     const hashPassword = await Auth.hashPassword(rawPassword);
     expect(rawPassword).not.toBe(hashPassword);
-    console.log('pw end')
     done();
   });
 
   it("should authenticate a valid user", async done => {
-    console.log('val start');
     const name = "Test User";
     const rawPassword = "Secret123";
     const email = "test@test.com";
@@ -47,7 +42,6 @@ describe("Auth Module", () => {
     expect(await userManager.getUserById(newUserData._id)).toEqual(
       loggedUserData
     );
-    console.log('val end');
     done();
   });
 });
