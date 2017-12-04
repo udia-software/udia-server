0b9be58adbe286f165c08a85c1e41422b435b9a9
Alexander Wong <admin@alexander-wong.com>
2017-12-03 22:44:23 -0700

    env variable MONGODB_URI

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const NODE_ENV = process.env.NODE_ENV || "development";
 const JWT_SECRET = process.env.JWT_SECRET || "UDIA Development JWT Secret Key";
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/udia";
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/udia";
 const PORT = process.env.port || 3000;
 const SALT_ROUNDS = 12;
 const TEST_JWT = process.env.TEST_JWT || "";
