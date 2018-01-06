059d79669ccf407f0561b7e5b2d1a83ab9d3052a
Alexander Wong <admin@alexander-wong.com>
2018-01-06 16:42:26 -0700

    Made app work with node mongodb 3.x

--- src/connectMongo.js
+++ src/connectMongo.js
@@ -2,7 +2,6 @@
 
 const { MongoClient } = require("mongodb");
 const { MONGODB_URI } = require("./constants");
-const { NODE_ENV } = require("./constants");
 
 // coverage don't care about db connection code
 /* istanbul ignore next */
@@ -10,21 +9,7 @@ const { NODE_ENV } = require("./constants");
  * Connect to the mongo daemon and return the database client instance.
  */
 const connectMongo = async () => {
-  if (NODE_ENV === "test") {
-    return await MongoClient.connect(MONGODB_URI + "_test");
-  }
   return await MongoClient.connect(MONGODB_URI);
-  // Development Performance Logging
-  // const { Logger } = require("mongodb");
-  // if (NODE_ENV === "development") {
-  //   let logCount = 0;
-  //   Logger.setCurrentLogger(msg => {
-  //     // eslint-disable-next-line no-console
-  //     console.log(`MONGO DB REQUEST ${++logCount}: ${msg}`);
-  //   });
-  //   Logger.setLevel("debug");
-  //   Logger.filter("class", ["Cursor"]);
-  // }
 };
 
 module.exports = connectMongo;
