d281ef8aecc2e3850838961d5eb969118b918730
Alexander Wong <admin@alexander-wong.com>
2018-02-20 20:32:10 -0700

    added failing subscription test

--- __test__/schema/subscriptions.test.js
+++ __test__/schema/subscriptions.test.js
@@ -19,25 +19,24 @@ const GRAPHQL_HTTP_ENDPOINT = `http://localhost:${PORT}/graphql`;
 let server = null;
 let networkInterface = null;
 let client = null;
-let resolverUser = null;
-let jwt = null;
 
 beforeAll(async done => {
   await testHelper.initializeTestState();
-  resolverUser = await testHelper.createTestUser({
+  const resolverUser = await testHelper.createTestUser({
     username: "subscriber",
     rawPassword: "supersecret123",
     email: "subscription@test.com"
   });
-  jwt = await testHelper.getJWT({
+
+  const jwt = await testHelper.getJWT({
     rawPassword: "supersecret123",
-    email: "subscription@test.com"
+    email: resolverUser.email
   });
 
   server = await start();
   networkInterface = new SubscriptionClient(
     GRAPHQL_SUBSCRIPTIONS_ENDPOINT,
-    { reconnect: true, connectionParams: { authToken: jwt } },
+    { reconnect: true, connectionParams: { authorization: jwt } },
     WebSocket
   );
 
@@ -64,99 +63,79 @@ afterAll(async done => {
 });
 
 describe("Subscriptions", () => {
-  it("should subscribe to node creations");
-  // it("should subscribe to node creations", async done => {
-  //   const nodeSubscriptionPromise = new Promise((resolve, reject) => {
-  //     client
-  //       .subscribe({
-  //         query: gql`
-  //           subscription NodeSubcription {
-  //             NodeSubscription(filter: { mutation_in: [CREATED] }) {
-  //               mutation
-  //               node {
-  //                 title
-  //               }
-  //             }
-  //           }
-  //         `
-  //       })
-  //       .subscribe({
-  //         next: resolve,
-  //         error: reject
-  //       });
-  //   });
+  it("should subscribe to node creations", async done => {
+    const subscriptionPromise = new Promise((resolve, reject) => {
+      client
+        .subscribe({
+          query: gql`
+            subscription AllNodesSubcription {
+              NodeSubscription(filter: { mutation_in: [CREATED] }) {
+                mutation
+                node {
+                  title
+                }
+              }
+            }
+          `
+        })
+        .subscribe({
+          next: resolve,
+          error: reject
+        });
+    });
 
-  //   const mutationResponse = await client.mutate({
-  //     mutation: gql`
-  //       mutation createNode(
-  //         $dataType: NodeDataType!
-  //         $relationType: NodeRelationType!
-  //         $title: String!
-  //         $content: String!
-  //         $parentId: ID
-  //       ) {
-  //         createNode(
-  //           dataType: $dataType
-  //           relationType: $relationType
-  //           title: $title
-  //           content: $content
-  //           parentId: $parentId
-  //         ) {
-  //           dataType
-  //           relationType
-  //           title
-  //           content
-  //           parent {
-  //             _id
-  //           }
-  //           children {
-  //             _id
-  //           }
-  //           createdBy {
-  //             _id
-  //           }
-  //           updatedBy {
-  //             _id
-  //           }
-  //         }
-  //       }
-  //     `,
-  //     variables: {
-  //       dataType: "TEXT",
-  //       relationType: "POST",
-  //       title: "Test Create Node Resolver",
-  //       content: "Test Create Node Resolver Content String!"
-  //     }
-  //   });
-
-  //   expect(mutationResponse).toEqual({
-  //     data: {
-  //       createNode: {
-  //         __typename: "Node",
-  //         children: [],
-  //         content: "Test Create Node Resolver Content String!",
-  //         createdBy: { __typename: "User", _id: "" + resolverUser._id },
-  //         dataType: "TEXT",
-  //         parent: null,
-  //         relationType: "POST",
-  //         title: "Test Create Node Resolver",
-  //         updatedBy: { __typename: "User", _id: "" + resolverUser._id }
-  //       }
-  //     }
-  //   });
-
-  //   // ASSERT SUBSCRIPTION RECEIVED EVENT
-  //   let subscriptionResult = await nodeSubscriptionPromise;
-  //   expect(subscriptionResult).toEqual({
-  //     data: {
-  //       NodeSubscription: {
-  //         mutation: "CREATED",
-  //         node: {
-  //           title: "Test Create Node Resolver"
-  //         }
-  //       }
-  //     }
-  //   });
-  //   done();
-  // });
+    await client.mutate({
+      mutation: gql`
+        mutation createNode(
+          $dataType: NodeDataType!
+          $relationType: NodeRelationType!
+          $title: String!
+          $content: String!
+          $parentId: ID
+        ) {
+          createNode(
+            dataType: $dataType
+            relationType: $relationType
+            title: $title
+            content: $content
+            parentId: $parentId
+          ) {
+            dataType
+            relationType
+            title
+            content
+            parent {
+              _id
+            }
+            children {
+              _id
+            }
+            createdBy {
+              _id
+            }
+            updatedBy {
+              _id
+            }
+          }
+        }
+      `,
+      variables: {
+        dataType: "TEXT",
+        relationType: "POST",
+        title: "Test Create Node Resolver",
+        content: "Test Create Node Resolver Content String!"
+      }
+    });
+    expect(await subscriptionPromise).toEqual({
+      data: {
+        NodeSubscription: {
+          mutation: "CREATED",
+          node: {
+            title: "Test Create Node Resolver"
+          }
+        }
+      }
+    });
+    done();
+  });
 });
